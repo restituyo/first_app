e6467a84ea11f5c2f18aea61115a22a241c46bf7
Byron <byron.restituyo@gmail.com>
2014-04-16 17:07:23 -0400

    not sure

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
+
 # Pick the frameworks you want:
 require "active_record/railtie"
 require "action_controller/railtie"
@@ -38,6 +39,8 @@ module SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.serve_static_assets = true
+
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
